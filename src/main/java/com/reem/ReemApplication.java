dbc85387fd102b080470b7ca6f827dd4308e6090
Nduduzo Komo <Nduudzo6@discovery.co.za>
2023-08-18 21:15:07 +0200

    first commit -base

---
+++ src/main/java/com/reem/ReemApplication.java
@@ -0,0 +1,13 @@
+package com.reem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reem/ReemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
